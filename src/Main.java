e0270393c142046be71248b545c49d9603edbf19
Michal Stanislav <sajsl@seznam.cz>
2023-07-19 19:38:29 +0200

    Přidán správný kód.
    Přidal jsem správný kód místo původního "HelloWorld".

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.net.InetAddress;
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String nameAndSurname = "Michal Stanislav";
+        LocalDate sellersDateOfBirth = LocalDate.of(1990,12,22);
+        int numberOfContracts = 50;
+        int tonsSold = 2000;
+        String sellersHeadquarter = "Prague";
+        String licensePlate = "5AZ 9366";
+        double vehicleConsumption = 5.5;
+        String ipAddress = "94.142.238.75";
+
+
+        double carrotsSoldPerContract = tonsSold/numberOfContracts;
+
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu je: " + carrotsSoldPerContract+ " tun.");
     }
 }
